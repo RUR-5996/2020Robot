3e603f955fc31bbc9ff18bb4b9fc90317aa876c8
Otakar Kořínek <46106489+OTK87@users.noreply.github.com>
2020-02-13 16:51:04 +0100

    Add files via upload
    Adds code for WheelOfFortune

---
+++ Constants.java
@@ -0,0 +1,47 @@
+package frc.robot;
+
+public class Constants {
+
+    //Limelight Constants
+    public static double limelightDefault = 0;
+    public enum limelightMode {
+        visionProcessing,
+        camera
+    }
+
+    //Encoder constants
+    public static double leftDriveTicks = 0;
+    public static double rightDriveTicks = 0;
+    public static double leftDriveDist = 0;
+    public static double rightDriveDist = 0;
+    
+    //Servo constants
+    public static final int servoInputChannel = 1;
+    public static final double servoPosition = 0; //sets the initial position for the servo
+    public static final double servoAngle = 0; //sets the initial angle for the servo
+
+    public static double[] leftPos = {};
+    public static double[] leftVel = {};
+    public static double[] leftAcc = {};
+    public static double[] rightPos = {};
+    public static double[] rightVel = {};
+    public static double[] rightAcc = {};
+
+    //DO NOT TOUCH UNLESS YOU KNOW WHAT R U DOING
+    public static final double deadzone = 0.15;//could be 0.2
+    public static final double leftDriveRatio = 0; //ticks per rot
+    public static final double rightDriveRatio = 0; //ticks per rot
+    public static final double driveWheelRadius = 0.1524; //meters
+
+    public static final double turnKP = 0.0;
+    public static final double turnKI = 0.0;
+    public static final double turnKD = 0.0;
+    public static final double turnspeed = 1; //motor output multiplier of turn PID
+
+    public static final double autokP = 1.23;
+    public static final double autokI = 0; //not being used
+    public static final double autokD = 0.001; //minimal difference
+    public static final double autokV = 1/1.3; //1/maxspeed
+    public static final double autokA = 0; //0.12
+    
+}
---
+++ Diagnostics.java
@@ -0,0 +1,103 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.I2C;
+import edu.wpi.first.wpilibj.Timer;
+import com.revrobotics.ColorSensorV3;
+
+public class Diagnostics {
+
+    RobotMap robotMap;
+    Timer measure;
+    ColorSensorV3 colorSensor = new ColorSensorV3(I2C.Port.kOnboard);
+    String color = "";
+
+    public Diagnostics(RobotMap robotMap) {
+        this.robotMap = robotMap;
+    }
+
+    public void send() {
+        SmartDashboard.putNumber("left drive ticks", Constants.leftDriveTicks);
+        SmartDashboard.putNumber("right drive ticks", Constants.rightDriveTicks);
+        SmartDashboard.putNumber("left drive distance", Constants.leftDriveDist);
+        SmartDashboard.putNumber("right drive distance", Constants.rightDriveDist);
+        SmartDashboard.putString("color sensor", color);
+    }
+
+    public void periodic() {
+        getDriveTicks();
+        calcDriveDist();
+        send();
+    }
+
+    public void getDriveSpeed() { // toto neberte v úvahu, poutze pro testovací účely
+        boolean timer = false;
+        double measureTime = 0;
+        resetDriveEncoders();
+        if(robotMap.buttonX.get()&&!timer) {
+            timer = true;
+            measure.start();
+        }
+        if(robotMap.buttonX.get()&&timer&&measure.get() >= 0.25) {
+            timer = false;
+            measureTime = measure.get();
+            measure.reset();
+            measure.stop();
+            
+            double velL = Constants.leftDriveDist / measureTime;
+            double velR = Constants.rightDriveDist / measureTime;
+            SmartDashboard.putNumber("velocity L", velL);
+            SmartDashboard.putNumber("velocity R", velR);
+        }
+
+    }
+
+    public void measureDriveAcc() { // toto neberte v úvahu, poutze pro testovací účely
+        resetDriveEncoders();
+        boolean timer = false;
+        double measureTime;
+        if(robotMap.buttonY.get()&&!timer) {
+            timer = true;
+            measure.start();
+        }
+        if(timer&&measure.get() >= 0.75) {
+            timer = false;
+            measureTime = measure.get();
+            measure.reset();
+            measure.stop();
+
+            double accL = Constants.leftDriveDist / (measureTime * measureTime);
+            double accR = Constants.rightDriveDist / (measureTime * measureTime);
+            SmartDashboard.putNumber("acceleration L", accL);
+            SmartDashboard.putNumber("acceleration R", accR);
+        }
+    }
+
+    public void getColor() {
+        if(colorSensor.getColor().toString().substring(37, 38).equals("4")) {
+            color = "blue";
+        } else if(colorSensor.getColor().toString().substring(37, 38).equals("6")) {
+            color = "green";
+        } else if(colorSensor.getColor().toString().substring(37, 38).equals("c")) {
+            color = "red";
+        } else if(colorSensor.getColor().toString().substring(37, 38).equals("9")) {
+            color = "yellow";
+        }
+    }
+
+    public void resetDriveEncoders() {
+        robotMap.leftFrontTalon.setSelectedSensorPosition(0);
+        robotMap.rightFrontTalon.setSelectedSensorPosition(0);
+    }
+
+    public void getDriveTicks() {
+        Constants.leftDriveTicks = robotMap.leftFrontTalon.getSelectedSensorPosition();
+        Constants.rightDriveTicks = robotMap.rightFrontTalon.getSelectedSensorPosition();
+    }
+
+    public void calcDriveDist() {
+        Constants.leftDriveDist = (Constants.leftDriveTicks/Constants.leftDriveRatio) * 2 * Math.PI * Constants.driveWheelRadius;
+        Constants.rightDriveDist = (Constants.rightDriveTicks/Constants.rightDriveRatio) * 2 * Math.PI * Constants.driveWheelRadius;
+    }
+
+}
